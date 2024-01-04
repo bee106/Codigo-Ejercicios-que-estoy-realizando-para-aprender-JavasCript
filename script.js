78f1a549e228f51a0a599ac4751c21700ff3f0d6
bee106 <davidocano52@gmail.com>
2024-01-04 07:07:11 -0500

    Primer Gitignore y primeros archivos del repositorio

---
+++ script.js
@@ -0,0 +1,41 @@
+// const title = document.querySelector('h1')
+// const p = document.querySelector('p')
+// const parrafito = document.querySelector('.parrafito')
+// const pid = document.getElementById('pid')
+// const input = document.querySelector('input')
+// console.log(input.value)
+// console.log(title)
+
+// title.innerHTML = 'Patito'
+// parrafito.getAttribute('class')
+// parrafito.setAttribute('class', 'rojo')
+// parrafito.classList.add('perro')
+
+
+// parrafito.getAttribute('class')
+// parrafito.setAttribute('class','pato')
+// parrafito.classList.add('code_img')
+// parrafito.classList.remove('code_img')
+
+// input.placeholder = "Juan David Ocaño Huertas"
+
+// const img = document.createElement('img')
+// img.setAttribute('src', './img.jpg')
+
+// pid.append(img)
+
+
+const num1 = document.querySelector('#calculo')
+const num2 = document.querySelector('#calculo2')
+const btn = document.querySelector('#btnCalcular')
+const pResult = document.querySelector('#resultado')
+const form = document.querySelector('#form')
+
+//     form.addEventListener('submit',sumarCal)
+// // btn.addEventListener('click', sumarCal)
+// function sumarCal(event){
+//     event.preventDefault()
+//     const reslutadoSuma = parseInt(num1.value) + parseInt(num2.value)
+//     pResult.innerHTML = 'el resultado es:'+ reslutadoSuma
+// }
+
